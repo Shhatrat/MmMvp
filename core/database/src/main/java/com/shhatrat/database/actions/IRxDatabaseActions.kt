277d606e7edca8218c0b837e1310bcb2e5b4cea1
Szymon Kaminski <szymonkaminski.ptb@gmail.com>
2021-04-05 02:09:55 +0200

    fix generic type letter

--- core/database/src/main/java/com/shhatrat/database/actions/IRxDatabaseActions.kt
+++ core/database/src/main/java/com/shhatrat/database/actions/IRxDatabaseActions.kt
@@ -3,9 +3,9 @@ package com.shhatrat.database.actions
 import io.reactivex.rxjava3.core.Observable
 import kotlin.reflect.KClass
 
-interface IRxDatabaseActions<Convertable> {
+interface IRxDatabaseActions<C> {
 
-    fun observeAll(realmClass: KClass<out Any>): Observable<List<Convertable>>
+    fun observeAll(realmClass: KClass<out Any>): Observable<List<C>>
 
-    fun observe(realmClass: KClass<out Any>, id: Any): Observable<Convertable>
+    fun observe(realmClass: KClass<out Any>, id: Any): Observable<C>
 }
