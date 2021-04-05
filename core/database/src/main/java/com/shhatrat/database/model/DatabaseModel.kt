0b94d63173ace16b1816d3ff145a0ba29451d904
Szymon Kaminski <szymonkaminski.ptb@gmail.com>
2021-04-05 02:10:32 +0200

    remove unused model

--- core/database/src/main/java/com/shhatrat/database/model/DatabaseModel.kt
+++
@@ -1,9 +0,0 @@
-package com.shhatrat.database.model
-
-abstract class DatabaseModel<NormalType, DatabaseType> {
-
-    abstract val value: NormalType
-
-    abstract fun DatabaseType.fromDatabase(): NormalType
-    abstract fun NormalType.toDatabase(): DatabaseType
-}
